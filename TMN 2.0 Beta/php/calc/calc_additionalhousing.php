bee8bda5dec34e26dd44a9937646c62426939b3c
Harro <michael.harrison@ccca.org.au>
2010-04-14 06:51:56 +0000

    fixes additional housing to handle fortnightly payments
    git-svn-id: svn://10.32.16.4/svn/tmn/trunk@11 46dc093c-65c8-4d49-8b1f-495a7a743764

--- TMN 2.0 Beta/php/calc/calc_additionalhousing.php
+++ TMN 2.0 Beta/php/calc/calc_additionalhousing.php
@@ -1,9 +1,11 @@
 <?php
 
-function calculateAdditionalHousing($housing, $spouse){
+function calculateAdditionalHousing($housing, $freq, $spouse){
 
 	//TODO grab MAX HOUSING MFB from DB
 	$maxhousingmfb = ($spouse ? 1600 : 960);
+	
+	if ($freq == 1) $housing = $housing * 26 / 12;
 	
 	return max( 0, $housing - $maxhousingmfb );
 }
