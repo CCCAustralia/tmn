dcc753cf609de60d5a49a2d487a9d4af8e003609
Harro <michael.harrison@ccca.org.au>
2010-04-14 02:58:40 +0000

    test
    git-svn-id: svn://10.32.16.4/svn/tmn/trunk@3 46dc093c-65c8-4d49-8b1f-495a7a743764

--- TMN 2.0 Beta/index.php
+++ TMN 2.0 Beta/index.php
@@ -1,5 +1,5 @@
 <?php
-
+/***
 /*******************************************
 #                                                             
 # index.php - Generic index for PHP GCX SSO login                    
