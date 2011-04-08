a83bd588bd0e4ade96fcc441e14b10719420004c
Harro <michael.harrison@ccca.org.au>
2011-04-08 03:43:08 +0000

    updated script to account for changed custom classes folder structure
    git-svn-id: svn://10.32.16.4/svn/tmn/trunk@278 46dc093c-65c8-4d49-8b1f-495a7a743764

--- TMN/ftp_script.sh
+++ TMN/ftp_script.sh
@@ -40,7 +40,8 @@ echo 'Compressing ExtJS Files ...'
 cat ext-base.js ext-all.js > ext.js
 
 echo 'Compressing Custom Library Files ...'
-cat DateRangeValidationType.js statusbar/StatusBar.js statusbar/ValidationStatus.js Printer-all.js iconcombo/Ext.ux.IconCombo.js > custom-libraries-all_long.js
+cd customclasses
+cat Ext.LinkButton.js DateRangeValidationType.js statusbar/StatusBar.js statusbar/ValidationStatus.js Printer-all.js Ext.ux.IconCombo.js > custom-libraries-all_long.js
 java -jar /Applications/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar -o custom-libraries-all.js custom-libraries-all_long.js
 rm custom-libraries-all_long.js
 
