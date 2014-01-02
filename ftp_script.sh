f25868bceb5c906db052c689a33b671b86286d52
Harro <michael.harrison@cru.org>
2014-01-02 15:43:37 -0500

    added compressor to repo. updated upload script.

--- ftp_script.sh
+++ ftp_script.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 
 version='2.4'
 repo_url='git@10.32.16.4:/srv/repos/git/tmn.git'
-yuicompressor_path='/Applications/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar'
+yuicompressor_path='lib/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar'
 ftp_uname='mportal'
 ftp_pword='***REMOVED***'
 ftp_destination='TMN'
